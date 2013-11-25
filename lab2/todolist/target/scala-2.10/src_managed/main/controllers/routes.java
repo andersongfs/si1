5f8cdd691b87b0d052e6146ff347392056e9901c
andersongfs <andersongfs@gmail.com>
2013-11-25 12:46:03 -0300

    todolist

---
+++ lab2/todolist/target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/Users/andersongfs/Documents/projetosPlay/todolist/conf/routes
+// @HASH:10faaf6b1c003369d3d38ad1091233d097ace0d7
+// @DATE:Mon Nov 25 09:15:29 BRT 2013
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+}
+          
